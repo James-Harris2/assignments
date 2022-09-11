9b46ed8b081587c95ac1bf50ee958e44fb9886f9
James Harris <jameseharrisii@gmail.com>
2022-09-11 13:15:43 -0700

    initial push

--- VS-Assignments/main.js
+++
@@ -1,8 +0,0 @@
-let screen= document.getElementById('screen');
-buttons = document.querySelectorAll('button');
-for(item of buttons){
-    item.addEventListener('click', (e)=>{
-        buttonText = e.target.innterText;
-        console.log('Button text is ', buttonText)
-    })
-}
